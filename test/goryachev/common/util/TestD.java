beb1b8f51ca404613477fac5a2754e9c62a526a6
andy <andy@goryachev.com>
2016-10-08 12:16:03 -0700

    d

---
+++ test/goryachev/common/util/TestD.java
@@ -0,0 +1,25 @@
+// Copyright © 2016 Andy Goryachev <andy@goryachev.com>
+package goryachev.common.util;
+import goryachev.common.test.TF;
+import goryachev.common.test.Test;
+
+
+/**
+ * Test D.
+ */
+public class TestD
+{
+	public static void main(String[] args)
+	{
+		TF.run();
+	}
+	
+	
+	@Test
+	public void test()
+	{
+		D.print("yo");
+		D.print("yo");
+		D.print("yo");
+	}
+}
